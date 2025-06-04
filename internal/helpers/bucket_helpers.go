af799a0fb7cf8dadcde05d9f1f082958b390bb85
Dan Field <dan.field@lyrasis.org>
2025-06-04 16:03:08 +0100

    code cleanup and inventory configuration

--- internal/helpers/bucket_helpers.go
+++ internal/helpers/bucket_helpers.go
@@ -121,14 +121,10 @@ func GetBuckets(ctx context.Context, s3Client *s3.Client, bucket string, key str
 	return buckets
 }
 
-// getBuckets retrieves a list of valid bucket names from an S3 object, validates them, and enforces a maximum limit.
-func CreateBucket(ctx context.Context, s3Client *s3.Client, bucketName string, bucketPrefix string) {
-
-	fullBucketName := fmt.Sprintf("%s-%s", bucketPrefix, bucketName)
-	log.Printf("Creating bucket  %v", fullBucketName)
-	region := os.Getenv("AWS_REGION") 
+func CreateNewBucket(ctx context.Context, s3Client *s3.Client, bucketName string) {
+	region := os.Getenv("AWS_REGION")
 	_, err := s3Client.CreateBucket(ctx, &s3.CreateBucketInput{
-		Bucket: aws.String(fullBucketName),
+		Bucket: aws.String(bucketName),
 		CreateBucketConfiguration: &types.CreateBucketConfiguration{
             LocationConstraint: types.BucketLocationConstraint(region),
         },
@@ -136,13 +132,15 @@ func CreateBucket(ctx context.Context, s3Client *s3.Client, bucketName string, b
 	if err != nil {
 		log.Fatalf("failed to create bucket: %v", err)
 	}
+}
 
-	_, err = s3Client.PutBucketTagging(ctx, &s3.PutBucketTaggingInput{
-		Bucket: aws.String(fullBucketName),
+func AddBucketTags(ctx context.Context, s3Client *s3.Client, bucketName string, stackName string) {
+	_, err := s3Client.PutBucketTagging(ctx, &s3.PutBucketTaggingInput{
+		Bucket: aws.String(bucketName),
 		Tagging: &types.Tagging{
             TagSet: []types.Tag{
                 {Key: aws.String("Application"), Value: aws.String("Duracloud")},
-                {Key: aws.String("StackName"), Value: aws.String(bucketPrefix)},
+                {Key: aws.String("StackName"), Value: aws.String(stackName)},
                 {Key: aws.String("BucketType"), Value: aws.String("Standard")},
             },
         },
@@ -152,6 +150,9 @@ func CreateBucket(ctx context.Context, s3Client *s3.Client, bucketName string, b
 	}
 	log.Printf("Bucket Tags added")
 
+}
+
+func AddDenyAllPolicy(ctx context.Context, s3Client *s3.Client, bucketName string) {
 	log.Printf("Bucket Deny-all policy generating")
 	// apply a default deny-all policy
 	policy := map[string]interface{}{
@@ -162,7 +163,7 @@ func CreateBucket(ctx context.Context, s3Client *s3.Client, bucketName string, b
                 "Effect":   "Deny",
                 "Principal": "*",
                 "Action":   "s3:PutObject",
-                "Resource": fmt.Sprintf("arn:aws:s3:::%s/*", fullBucketName),
+                "Resource": fmt.Sprintf("arn:aws:s3:::%s/*", bucketName),
             },
         },
     }
@@ -172,19 +173,20 @@ func CreateBucket(ctx context.Context, s3Client *s3.Client, bucketName string, b
         log.Fatalf("failed to marshal policy: %v", err)
     }
 
-    _, err = s3Client.PutBucketPolicy(ctx, &s3.PutBucketPolicyInput{
-        Bucket: aws.String(fullBucketName),
+	_, err = s3Client.PutBucketPolicy(ctx, &s3.PutBucketPolicyInput{
+        Bucket: aws.String(bucketName),
         Policy: aws.String(string(policyJSON)),
     })
     if err != nil {
         log.Fatalf("failed to put bucket policy: %v", err)
     }
-
 	log.Printf("Applied Bucket Deny-all policy")
+}
 
+func EnableVersioning(ctx context.Context, s3Client *s3.Client, bucketName string) {
 	log.Printf("Enable Bucket versioning")
-	_, err = s3Client.PutBucketVersioning(ctx, &s3.PutBucketVersioningInput{
-		Bucket: aws.String(fullBucketName),
+	_, err := s3Client.PutBucketVersioning(ctx, &s3.PutBucketVersioningInput{
+		Bucket: aws.String(bucketName),
 		VersioningConfiguration: &types.VersioningConfiguration{
 			Status: types.BucketVersioningStatusEnabled,
 		},
@@ -193,12 +195,13 @@ func CreateBucket(ctx context.Context, s3Client *s3.Client, bucketName string, b
 		log.Fatalf("failed to enable versioning: %v", err)
 	}
 	log.Println("Bucket versioning enabled.")
+}
 
-
+func AddExpiration(ctx context.Context, s3Client *s3.Client, bucketName string) {
 	days := int32(7)
 	log.Printf("Bucket Non-current expiration being set to %d days", days)
-	_, err = s3Client.PutBucketLifecycleConfiguration(ctx, &s3.PutBucketLifecycleConfigurationInput{
-		Bucket: aws.String(fullBucketName),
+	_, err := s3Client.PutBucketLifecycleConfiguration(ctx, &s3.PutBucketLifecycleConfigurationInput{
+		Bucket: aws.String(bucketName),
 		LifecycleConfiguration: &types.BucketLifecycleConfiguration{
 			Rules: []types.LifecycleRule{
 				{
@@ -216,98 +219,169 @@ func CreateBucket(ctx context.Context, s3Client *s3.Client, bucketName string, b
 		log.Fatalf("failed to set lifecycle rule: %v", err)
 	}
 	log.Printf("Lifecycle rule set: Non-current versions expire after %d days", days)
+}
 
-	if IsPublicBucket(bucketName) {
-		log.Printf("Public Bucket detected")
-		blockFalse := false
-		_, err := s3Client.PutPublicAccessBlock(ctx, &s3.PutPublicAccessBlockInput{
-			Bucket: aws.String(fullBucketName),
-			PublicAccessBlockConfiguration: &types.PublicAccessBlockConfiguration{
-				BlockPublicAcls:       &blockFalse,
-				IgnorePublicAcls:      &blockFalse,
-				BlockPublicPolicy:     &blockFalse,
-				RestrictPublicBuckets: &blockFalse,
+func MakePublic(ctx context.Context, s3Client *s3.Client, bucketName string) {
+	log.Printf("Public Bucket detected")
+	blockFalse := false
+	_, err := s3Client.PutPublicAccessBlock(ctx, &s3.PutPublicAccessBlockInput{
+		Bucket: aws.String(bucketName),
+		PublicAccessBlockConfiguration: &types.PublicAccessBlockConfiguration{
+			BlockPublicAcls:       &blockFalse,
+			IgnorePublicAcls:      &blockFalse,
+			BlockPublicPolicy:     &blockFalse,
+			RestrictPublicBuckets: &blockFalse,
+		},
+	})
+	if err != nil {
+		log.Fatalf("failed to disable public access block: %v", err)
+	}
+	log.Println("Public access block disabled.")
+}
+
+func AddPublicPolicy(ctx context.Context, s3Client *s3.Client, bucketName string) {
+	policy := map[string]interface{}{
+		"Version": "2012-10-17",
+		"Statement": []map[string]interface{}{
+			{
+				"Sid":       "AllowPublicRead",
+				"Effect":    "Allow",
+				"Principal": "*",
+				"Action":    "s3:GetObject",
+				"Resource":  fmt.Sprintf("arn:aws:s3:::%s/*", bucketName),
 			},
-		})
-		if err != nil {
-			log.Fatalf("failed to disable public access block: %v", err)
-		}
-		log.Println("Public access block disabled.")
+		},
+	}
 
+	policyJSON, err := json.Marshal(policy)
+	if err != nil {
+		log.Fatalf("failed to marshal bucket policy: %v", err)
+	}
 
-		policy := map[string]interface{}{
-			"Version": "2012-10-17",
-			"Statement": []map[string]interface{}{
+	_, err = s3Client.PutBucketPolicy(ctx, &s3.PutBucketPolicyInput{
+		Bucket: aws.String(bucketName),
+		Policy: aws.String(string(policyJSON)),
+	})
+	if err != nil {
+		log.Fatalf("failed to apply public bucket policy: %v", err)
+	}
+	log.Println("Public bucket policy applied.")
+}
+
+func AddPublicTags(ctx context.Context, s3Client *s3.Client, bucketName string) {
+	_, err := s3Client.PutBucketTagging(ctx, &s3.PutBucketTaggingInput{
+		Bucket: aws.String(bucketName),
+		Tagging: &types.Tagging{
+			TagSet: []types.Tag{
+				{Key: aws.String("BucketType"), Value: aws.String("Public")},
+			},
+		},
+	})
+	if err != nil {
+		log.Fatalf("failed to add  bucket tags: %v", err)
+	}
+	log.Printf("Bucket Tags added")
+}
+
+func EnableInventory(ctx context.Context, s3Client *s3.Client, bucketName string) {
+	var arn = os.Getenv("S3_REPLICATION_ROLE_ARN")
+	var destBucket = fmt.Sprintf("%s-%s", bucketName, IsManagedSuffix)
+    _, err := s3Client.PutBucketInventoryConfiguration(ctx, &s3.PutBucketInventoryConfigurationInput{
+        Bucket: aws.String(bucketName),
+        Id:     aws.String("InventoryReport"),
+        InventoryConfiguration: &types.InventoryConfiguration{
+			IsEnabled: aws.Bool(true),
+			Id:     aws.String("InventoryReport"),
+            IncludedObjectVersions: types.InventoryIncludedObjectVersionsAll,
+            Schedule: &types.InventorySchedule{
+                Frequency: types.InventoryFrequencyDaily,
+            },
+            Destination: &types.InventoryDestination{
+                S3BucketDestination: &types.InventoryS3BucketDestination{
+                    AccountId: aws.String(arn), // your AWS account ID
+                    Bucket:    aws.String("arn:aws:s3:::" + destBucket),
+                    Format:    types.InventoryFormatCsv,
+                    Prefix:    aws.String("inventory/"),
+                },
+            },
+            OptionalFields: []types.InventoryOptionalField{
+                types.InventoryOptionalFieldSize,
+                types.InventoryOptionalFieldLastModifiedDate,
+                types.InventoryOptionalFieldStorageClass,
+            },
+        },
+    })
+
+    if err != nil {
+        log.Fatalf("failed to enable inventory configuration: %v", err)
+    }
+
+    log.Println("Inventory configuration enabled.")
+}
+
+func EnableLifecycle(ctx context.Context, s3Client *s3.Client, bucketName string) {
+	daysToIA      := int32(30)
+	daysToGlacier := int32(60)
+
+	_, err := s3Client.PutBucketLifecycleConfiguration(ctx, &s3.PutBucketLifecycleConfigurationInput{
+		Bucket: aws.String(bucketName),
+		LifecycleConfiguration: &types.BucketLifecycleConfiguration{
+			Rules: []types.LifecycleRule{
 				{
-					"Sid":       "AllowPublicRead",
-					"Effect":    "Allow",
-					"Principal": "*",
-					"Action":    "s3:GetObject",
-					"Resource":  fmt.Sprintf("arn:aws:s3:::%s/*", fullBucketName),
-				},
-			},
-		}
-
-		policyJSON, err := json.Marshal(policy)
-		if err != nil {
-			log.Fatalf("failed to marshal bucket policy: %v", err)
-		}
-
-		_, err = s3Client.PutBucketPolicy(ctx, &s3.PutBucketPolicyInput{
-			Bucket: aws.String(fullBucketName),
-			Policy: aws.String(string(policyJSON)),
-		})
-		if err != nil {
-			log.Fatalf("failed to apply public bucket policy: %v", err)
-		}
-		log.Println("Public bucket policy applied.")
-
-		_, err = s3Client.PutBucketTagging(ctx, &s3.PutBucketTaggingInput{
-			Bucket: aws.String(fullBucketName),
-			Tagging: &types.Tagging{
-				TagSet: []types.Tag{
-					{Key: aws.String("BucketType"), Value: aws.String("Public")},
-				},
-			},
-		})
-		if err != nil {
-			log.Fatalf("failed to add  bucket tags: %v", err)
-		}
-		log.Printf("Bucket Tags added")
-
-
-	} else {
-		daysToIA      := int32(30)
-		daysToGlacier := int32(60)
-
-		_, err := s3Client.PutBucketLifecycleConfiguration(ctx, &s3.PutBucketLifecycleConfigurationInput{
-			Bucket: aws.String(fullBucketName),
-			LifecycleConfiguration: &types.BucketLifecycleConfiguration{
-				Rules: []types.LifecycleRule{
-					{
-						ID:     aws.String("IAThenGlacierIR"),
-						Status: types.ExpirationStatusEnabled,
-						Filter: &types.LifecycleRuleFilter{Prefix: aws.String("")}, // All objects
-						Transitions: []types.Transition{
-							{
-								Days:         &daysToIA,
-								StorageClass: types.TransitionStorageClassStandardIa,
-							},
-							{
-								Days:         &daysToGlacier,
-								StorageClass: types.TransitionStorageClassGlacierIr,
-							},
+					ID:     aws.String("IAThenGlacierIR"),
+					Status: types.ExpirationStatusEnabled,
+					Filter: &types.LifecycleRuleFilter{Prefix: aws.String("")}, // All objects
+					Transitions: []types.Transition{
+						{
+							Days:         &daysToIA,
+							StorageClass: types.TransitionStorageClassStandardIa,
+						},
+						{
+							Days:         &daysToGlacier,
+							StorageClass: types.TransitionStorageClassGlacierIr,
 						},
 					},
 				},
 			},
-		})
-
-		if err != nil {
-			log.Fatalf("failed to configure lifecycle: %v", err)
-		}
-
-		log.Printf("Lifecycle rule set: Transition to  IA in %d days, then Glacier IR after %d days.", daysToIA, daysToGlacier)
+		},
+	})
+	if err != nil {
+		log.Fatalf("failed to configure lifecycle: %v", err)
 	}
+	log.Printf("Lifecycle rule set: Transition to  IA in %d days, then Glacier IR after %d days.", daysToIA, daysToGlacier)
 
+}
+
+func EnableEventBridge(ctx context.Context, s3Client *s3.Client, bucketName string) {
+	_, err := s3Client.PutBucketNotificationConfiguration(ctx, &s3.PutBucketNotificationConfigurationInput{
+        Bucket: aws.String(bucketName),
+        NotificationConfiguration: &types.NotificationConfiguration{
+            EventBridgeConfiguration: &types.EventBridgeConfiguration{},
+        },
+    })
+    if err != nil {
+        log.Fatalf("failed to enable EventBridge notifications: %v", err)
+    }
+    log.Printf("EventBridge notifications enabled on bucket: %v", bucketName)
+}
+
+func CreateBucket(ctx context.Context, s3Client *s3.Client, bucketName string, stackName string) {
+
+	fullBucketName := fmt.Sprintf("%s-%s", stackName, bucketName)
+	log.Printf("Creating bucket  %v", fullBucketName)
+	CreateNewBucket(ctx, s3Client, fullBucketName)
+	AddBucketTags(ctx, s3Client, fullBucketName, stackName)
+	AddDenyAllPolicy(ctx, s3Client, fullBucketName)
+	EnableVersioning(ctx, s3Client, fullBucketName)
+	AddExpiration(ctx, s3Client, fullBucketName)
+
+	if IsPublicBucket(bucketName) {
+		MakePublic(ctx, s3Client, fullBucketName)
+		AddPublicPolicy(ctx, s3Client, fullBucketName)
+		AddPublicTags(ctx, s3Client, fullBucketName)
+	} else {
+		EnableLifecycle(ctx, s3Client, fullBucketName)
+	}
+	EnableEventBridge(ctx, s3Client, fullBucketName)
+	EnableInventory(ctx, s3Client, fullBucketName)
 }
