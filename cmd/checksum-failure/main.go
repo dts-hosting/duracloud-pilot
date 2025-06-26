86812d7485b5a8deb3b3fdb3e34c1f0abfe9d706
Mark Cooper <551470+mark-cooper@users.noreply.github.com>
2025-06-26 15:27:36 -0700

    Better baseline for checksum failure function

--- cmd/checksum-failure/main.go
+++ cmd/checksum-failure/main.go
@@ -2,33 +2,100 @@ package main
 
 import (
 	"context"
-	"log"
-
+	"duracloud/internal/accounts"
+	"duracloud/internal/notifications"
+	_ "embed"
+	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/sns"
+	"log"
+	"os"
+	"text/template"
 )
+
+var (
+	//go:embed templates/failure-notification.txt
+	notificationTemplate string
+
+	accountID        string
+	notificationTmpl *template.Template
+	snsClient        *sns.Client
+	snsTopicArn      string
+	stackName        string
+)
+
+func init() {
+	awsConfig, err := config.LoadDefaultConfig(context.Background())
+	if err != nil {
+		log.Fatalf("Unable to load AWS config: %v", err)
+	}
+
+	accountID, err = accounts.GetAccountID(context.Background(), awsConfig)
+	if err != nil {
+		log.Fatalf("Unable to get AWS account ID: %v", err)
+	}
+
+	notificationTmpl, err = template.New("notification").Parse(notificationTemplate)
+	if err != nil {
+		log.Fatalf("Failed to parse notification template: %v", err)
+	}
+
+	snsClient = sns.NewFromConfig(awsConfig)
+	snsTopicArn = os.Getenv("SNS_TOPIC_ARN")
+	stackName = os.Getenv("STACK_NAME")
+}
 
 func handler(ctx context.Context, event events.DynamoDBEvent) error {
 	for _, record := range event.Records {
-		if record.Change.NewImage != nil {
-			bucket := record.Change.NewImage["Bucket"].String()
-			object := record.Change.NewImage["Object"].String()
+		if record.Change.NewImage == nil {
+			log.Printf("Skipping checksum record failure with unexpected no NewImage: %v", record)
+			continue
+		}
 
-			if checksumSuccess, exists := record.Change.NewImage["LastChecksumSuccess"]; exists {
-				success := checksumSuccess.Boolean()
-				if !success {
-					log.Printf("CHECKSUM FAILURE DETECTED: %s/%s", bucket, object)
+		bucket := record.Change.NewImage["Bucket"].String()
+		object := record.Change.NewImage["Object"].String()
 
-					if lastError, exists := record.Change.NewImage["LastChecksumMessage"]; exists {
-						log.Printf("Error Details: %s", lastError.String())
-					}
+		checksumSuccess, exists := record.Change.NewImage["LastChecksumSuccess"]
+		if !exists {
+			log.Printf("Checksum status field not found for %s/%s", bucket, object)
+			continue
+		}
 
-					// TODO: handle failure
-					// - Upload report to s3 managed bucket?
-					// - Send SES email?
-					// - etc.
-				}
-			}
+		success := checksumSuccess.Boolean()
+		if success {
+			log.Printf("Checksum status is ok for %s/%s", bucket, object)
+			continue
+		}
+
+		log.Printf("Checksum failure detected: %s/%s", bucket, object)
+
+		errorMessage := "Unknown error"
+		if lastError, exists := record.Change.NewImage["LastChecksumMessage"]; exists {
+			errorMessage = lastError.String()
+		}
+
+		// TODO: handle failure
+		// - retry in case something unexpected happened?
+		// - Upload report to s3 managed bucket?
+		// - Send email?
+		// - etc.
+
+		notification := notifications.ChecksumFailureNotification{
+			Account:      accountID,
+			Bucket:       bucket,
+			Object:       object,
+			Date:         record.Change.ApproximateCreationDateTime.String(),
+			ErrorMessage: errorMessage,
+			Stack:        stackName,
+			Title:        fmt.Sprintf("DuraCloud Checksum Failure: %s/%s", bucket, object),
+			Template:     notificationTmpl,
+			Topic:        snsTopicArn,
+		}
+
+		if err := notifications.SendNotification(ctx, snsClient, notification); err != nil {
+			log.Printf("Failed to send checksum failure notification: %v", err)
 		}
 	}
 
---
+++ internal/notifications/notifications.go
@@ -0,0 +1,65 @@
+package notifications
+
+import (
+	"bytes"
+	"context"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/sns"
+	"log"
+	"text/template"
+)
+
+// SNSNotification represents an abstraction for a notification to be published via AWS SNS.
+type SNSNotification interface {
+	Message() (string, error)
+	Subject() string
+	TopicArn() string
+}
+
+type ChecksumFailureNotification struct {
+	Account      string
+	Bucket       string
+	Object       string
+	Date         string
+	ErrorMessage string
+	Stack        string
+	Title        string
+	Template     *template.Template
+	Topic        string
+}
+
+func (n ChecksumFailureNotification) Message() (string, error) {
+	var buf bytes.Buffer
+	if err := n.Template.Execute(&buf, n); err != nil {
+		return "", err
+	}
+	return buf.String(), nil
+}
+
+func (n ChecksumFailureNotification) Subject() string {
+	return n.Title
+}
+
+func (n ChecksumFailureNotification) TopicArn() string {
+	return n.Topic
+}
+
+func SendNotification(ctx context.Context, client *sns.Client, notification SNSNotification) error {
+	message, err := notification.Message()
+	if err != nil {
+		return err
+	}
+
+	result, err := client.Publish(ctx, &sns.PublishInput{
+		TopicArn: aws.String(notification.TopicArn()),
+		Subject:  aws.String(notification.Subject()),
+		Message:  aws.String(message),
+	})
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Notification sent successfully: %s", *result.MessageId)
+	return nil
+
+}
