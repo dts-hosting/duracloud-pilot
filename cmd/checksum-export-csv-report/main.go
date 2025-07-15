37064756e021152140a0dede66ea2a1e13401537
Dan Field <dan.field@lyrasis.org>
2025-07-15 16:55:09 +0100

    skeleton Lambda function

---
+++ cmd/checksum-export-csv-report/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"duracloud/internal/accounts"
+	"encoding/json"
+	"fmt"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"log"
+	"os"
+	"time"
+)
+
+var (
+	accountID         string
+	awsCtx            accounts.AWSContext
+	bucketPrefix      string
+	s3Client          *s3.Client
+	exportBucket      string
+	region            string
+	managedBucketName string
+)
+
+func init() {
+	awsConfig, err := config.LoadDefaultConfig(context.Background())
+	bucketPrefix = os.Getenv("S3_BUCKET_PREFIX")
+	managedBucketName = os.Getenv("S3_MANAGED_BUCKET")
+	timestamp := time.Now().Format("2006-01-02")
+	prefix := fmt.Sprintf("exports/checksum-table/%s/", timestamp)
+	s3Client = s3.NewFromConfig(awsConfig)
+	stackName := bucketPrefix
+
+	awsCtx = accounts.AWSContext{
+		AccountID: accountID,
+		Region:    region,
+		StackName: stackName,
+	}
+}
+
+func handler(ctx context.Context, event json.RawMessage) error {
+	ctx = context.WithValue(ctx, accounts.AWSContextKey, awsCtx)
+}
+
+func main() {
+	lambda.Start(handler)
+}
